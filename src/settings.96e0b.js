81903281d720de25bdbcd254b62f5fb290b00b9c
suwenbin <767418883@qq.com>
2023-02-10 19:49:11 +0800

    2

---
+++ src/settings.96e0b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"279e0",resources:"dff8b",main:"b7001"}};
