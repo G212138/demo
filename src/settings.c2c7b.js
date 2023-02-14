bc69c0d1a63fb2fb7311ee9308a39067e96306a6
suwenbin <767418883@qq.com>
2023-02-14 18:47:35 +0800

    1

---
+++ src/settings.c2c7b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/frame/scene/Teacher.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"803f8",frameRes:"1494e",resources:"3dfe9",main:"26fe6"}};
